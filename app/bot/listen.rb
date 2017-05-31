06ba86d55fb64e7117138e17cc64a3bd0bb71907
John Neil Soo <neil.soo@adish.co.jp>
2017-05-31 16:09:22 +0800

    issue #3 Added menu model for chat bot. still fixing bot logic

--- app/bot/listen.rb
+++ app/bot/listen.rb
@@ -7,10 +7,33 @@ Facebook::Messenger::Subscriptions.subscribe(
 )
 
 Bot.on :message do |msg|
-  Bot.deliver({
-    recipient: msg.sender,
-    message: {
-      text: msg.text
-    }
-  }, access_token: ENV['ACCESS_TOKEN'])
+  puts "@@@@@@@@@@@@@@@ #{msg.inspect}"
+  if msg.quick_reply == 'MENU'
+    msg.reply(
+      text: 'Choose from listed menu',
+      quick_replies: Menu.all.map do |menu|
+        {
+          context_type: 'text', 
+          title: "#{menu.name} - #{menu.price}",
+          payload: "#{menu.id}"
+        }
+      end
+    )
+  else
+    msg.reply(
+      text: 'We are almost ready to launch! Enter your details below to be one of the first ones in! Sign Up Now!',
+      quick_replies: [
+        {
+          content_type: 'text',
+          title: 'Menu',
+          payload: 'MENU'
+        },
+        {
+          content_type: 'text',
+          title: 'Sign Up',
+          payload: 'SIGN UP'
+        }
+      ]
+    )
+  end
 end
