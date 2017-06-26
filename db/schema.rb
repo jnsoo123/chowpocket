fffcb1da61fa8a0c35712b6b9b87e67e3ee794f9
jnsoo123 <johnneilsoo@gmail.com>
2017-06-26 11:48:43 +0800

    removed uid attribute from user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170624005748) do
+ActiveRecord::Schema.define(version: 20170626034639) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,7 +94,6 @@ ActiveRecord::Schema.define(version: 20170624005748) do
     t.datetime "updated_at", null: false
     t.string "role", default: "user"
     t.string "provider"
-    t.string "uid"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
