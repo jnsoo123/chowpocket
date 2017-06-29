ec931c417766abfc0bfda41898a23ee52a9bb872
John Neil Soo <neil.soo@adish.co.jp>
2017-06-29 14:11:57 +0800

    hotfix: trying to fix mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,10 +31,8 @@ Rails.application.configure do
   config.assets.compile = false
 
   config.action_mailer.default_url_options = { host: 'chowpocket.herokuapp.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
