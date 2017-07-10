bb8750deeb3748864ebc78a44e5c19cd1bfdaca3
jnsoo123 <johnneilsoo@gmail.com>
2017-07-10 20:06:54 +0800

    update fixes

--- app/jobs/send_confirmation_mail_job.rb
+++ app/jobs/send_confirmation_mail_job.rb
@@ -5,6 +5,6 @@ class SendConfirmationMailJob < ApplicationJob
     Order.pending.includes(cart: :user).map(&:user).each do |user|
       OrderMailer.order_confirmed(user).deliver
     end
-    Order.pending.update_all(status: OrderStatuses::CONFIRMED)
+    Order.pending_today.update_all(status: OrderStatuses::CONFIRMED)
   end
 end
