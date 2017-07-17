bd45d0518d383103955096b2f55abea5d0f80e84
John Neil Soo <neil.soo@adish.co.jp>
2017-07-17 15:16:49 +0800

    #18 Made datetime readable in order history page. changed default timezone to local

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module Chowpocket
     # Initialize configuration defaults for originally generated Rails version.
 
     config.load_defaults 5.1
+    config.time_zone = 'Asia/Taipei'
+    config.active_record.default_timezone = :local
 
     # -- END --
 
