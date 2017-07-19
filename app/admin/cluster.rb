9086756de714aba5b7321a3333ad90745188eccf
John Neil Soo <neil.soo@adish.co.jp>
2017-07-19 09:55:06 +0800

    #9 fixed displaying of clustered order in admin

--- app/admin/cluster.rb
+++ app/admin/cluster.rb
@@ -11,4 +11,20 @@ ActiveAdmin.register Cluster do
     column :discount
     actions
   end
+
+  show do
+    attributes_table do
+      row :id
+      row :menu
+      row :discount
+      row :date_created
+    end
+
+    panel 'Order Item List' do
+      table_for cluster.menu_clusters.includes(:order) do
+        column :order
+        column :quantity
+      end
+    end
+  end
 end
