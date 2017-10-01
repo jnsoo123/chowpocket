bbac73c95e5a58d3c6648c9a184fb257aaa0d6fa
jnsoo123 <johnneilsoo@gmail.com>
2017-10-01 23:46:10 +0800

    fixes #40

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,7 @@
 class Building < ApplicationRecord
-  has_many :users
+  has_many :users, dependent: :nullify
+
+  before_destroy do
+    self.users.update_all company_name: nil, floor: nil 
+  end
 end
