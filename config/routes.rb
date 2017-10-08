750ab4beed56e1582ab1c5a386f6028044468dff
jnsoo123 <johnneilsoo@gmail.com>
2017-10-08 19:47:44 +0800

    removed routing for facebook bot

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,6 @@ Rails.application.routes.draw do
 
   ActiveAdmin.routes(self)
 
-  mount Facebook::Messenger::Server, at: 'bot'
-
   resources :line_items, only: [:create, :update]
   resources :orders, only: [:create, :index, :show, :destroy]
   resources :checkouts, only: :index
