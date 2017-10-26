84bceea490094a8497fb1ca44f6142b895b9385d
jnsoo123 <johnneilsoo@gmail.com>
2017-10-26 20:59:10 +0800

    updated smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,13 +30,13 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.action_mailer.default_url_options = { host: 'chowpocket.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.chowpocket.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'chowpocket.herokuapp.com',
+    domain: 'www.chowpocket.com',
     authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV['GMAIL_USERNAME'],
